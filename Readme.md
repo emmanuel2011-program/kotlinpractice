c10a4890a0ab9f6c6b8fb8f800738fd8c8fce273
Emmanuel <okoyee61@gmail.com>
2026-03-16 09:50:39 +0100

    build console calculator

--- Readme.md
+++ Readme.md
@@ -2,22 +2,24 @@
 
 {Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
 
-{Provide a description of your software}
 
-{Describe your purpose for creating this software.}
+This project is a console-based Smart Calculator built with Kotlin.
 
-{Provide a link to your YouTube demonstration.  It should be a one minute demo of the software running and a walkthrough of the code.}
+The purpose of This project console-based Smart Calculator built with Kotlin is to help officials of the 3MTT computer distribution project so that users or officials can input arithmetic numbers and the calculator uses the operator selected by the user to complete the calculation.
 
-[Software Demo Video](http://youtube.link.goes.here)
+
+
+[Software Demo Video](http://youtube.link.goes.https://youtu.be/RgUQeVELMsQ)
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
 
-{Describe the programming language that you used}
+I used intellij as my code writer also downloading open jdk so as to develop the console base 3mtt calculator
+
+{kotlin is a modern programming language that is concise and safe, easy to learn, works on different platforms. can be used for mobile application, web development, server side application, data science and much more }
 
 # Useful Websites
 
-{Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+
+* [Web Site Name](http://url.link.goes.herhttps://w3schoolsua.github.io/kotlin/kotlin_getstarted_en.html#gsc.tab=0)
+* [Web Site Name](http://url.link.goes.herehttps://gemini.google.com/)
--- untitled/src/main/kotlin/Main.kt
+++ untitled/src/main/kotlin/Main.kt
@@ -1,16 +1,40 @@
-package org.example
+// this is user calculator where users can calculate arithmetic numbers
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("--- Smart 3MTT Calculator ---") // this will print what am about to code
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    while (true) { // This keeps the program running forever until you stop it
+        print("\nEnter first number (or type 'exit' to quit): ")
+        val input1 = readln()
+
+        if (input1.lowercase() == "exit") break // this lets the user leave when they type exit
+
+        val num1 = input1.toDoubleOrNull()
+        if (num1 == null) {
+            println("❌ Error: That's not a valid number. Try again.")
+            continue // this Skips the rest and starts the loop over
+        }
+
+        print("Enter an operator (+, -, *, /): ") // these are the operators the user will have to select
+        val op = readln()
+
+        print("Enter second number: ")       // this is the second number selected by the user
+        val num2 = readln().toDoubleOrNull()
+
+        if (num2 == null) {
+            println("❌ Error: Invalid second number.")
+            continue      // makes the code to continue running not to exit
+        }
+            // using val makes the input not to be changeable
+        val result = when (op) {
+            "+" -> num1 + num2
+            "-" -> num1 - num2
+            "*" -> num1 * num2
+            "/" -> if (num2 != 0.0) num1 / num2 else "Division by Zero Error!"
+            else -> "Unknown Operator"
+        }
+
+        println("✅ Result: $result")
     }
+    println("Goodbye!")
 }
